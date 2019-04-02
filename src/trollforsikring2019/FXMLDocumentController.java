bc4f1a5cd94d20188a7630c3b680c0997a715d72
aleksjoh <32634575+aleksjoh@users.noreply.github.com>
2019-04-02 17:13:10 +0200

    endringer

--- src/trollforsikring2019/FXMLDocumentController.java
+++ src/trollforsikring2019/FXMLDocumentController.java
@@ -10,13 +10,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 /**
  *
@@ -26,6 +22,15 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    
+    @FXML
+    private Button btnKunde; 
+    
+    @FXML
+    private Button btnSkademeld; 
+    
+    @FXML
+    private Button btnForsk; 
     
     @FXML
     private void handleButtonAction(ActionEvent event) throws IOException {

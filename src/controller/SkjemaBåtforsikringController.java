c2f08ae80d032158c768d98409cb141d8bf4a63a
relw <you@example.com>
2019-04-04 11:06:01 +0200

    la til border

--- src/controller/SkjemaBåtforsikringController.java
+++ src/controller/SkjemaBåtforsikringController.java
@@ -97,7 +97,6 @@ public class SkjemaBåtforsikringController implements Initializable {
           try{
             feilhåndteringBåtforsikringSkjema.sjekkInputForsikringspremie(Forsikringspremie);
             feilmeldingForsikringspremie.setText("OK");
-            
         }
         catch(feilhåndteringBåtforsikringSkjema.feilTallInnput ex){
             feilmeldingForsikringspremie.setText(ex.getMessage());

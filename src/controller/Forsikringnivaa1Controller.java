e7a3c5344a172222f3fddaa89a068feb7faa76bf
aleksjoh <32634575+aleksjoh@users.noreply.github.com>
2019-04-08 17:32:51 +0200

    Tilbakeknapp

--- src/controller/Forsikringnivaa1Controller.java
+++ src/controller/Forsikringnivaa1Controller.java
@@ -9,11 +9,13 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 /**
@@ -23,6 +25,9 @@ import javafx.stage.Stage;
  */
 public class Forsikringnivaa1Controller implements Initializable {
 
+    @FXML
+    private Button btnTilbake; 
+    
     public void nyBåtforsikringSkjema(ActionEvent event) throws IOException{
         Parent home_page_parent=FXMLLoader.load(getClass().getResource("/view/skjemaBåtforsikring.fxml"));
         Scene home_page_scene=new Scene(home_page_parent);
@@ -39,6 +44,15 @@ public class Forsikringnivaa1Controller implements Initializable {
     }
     public void nyReiseforsikringSkjema(ActionEvent event){
         
+    }
+    
+    @FXML 
+    public void tilbake(ActionEvent event) throws IOException{
+        Parent home_page_parent=FXMLLoader.load(getClass().getResource("/trollforsikring2019/FXMLDocument.fxml"));
+        Scene home_page_scene=new Scene(home_page_parent);
+        Stage app_stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_page_scene);
+        app_stage.show();
     }
     @Override
     public void initialize(URL url, ResourceBundle rb) {

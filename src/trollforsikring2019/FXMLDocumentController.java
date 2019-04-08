184434628b6e64b3b0fac9878e3d2c5bb9e3cdc5
aleksjoh <32634575+aleksjoh@users.noreply.github.com>
2019-04-08 17:09:38 +0200

    Stylet forsikringsnivå 1 og la til funksjonalitet til knappene på hovedsiden

--- src/trollforsikring2019/FXMLDocumentController.java
+++ src/trollforsikring2019/FXMLDocumentController.java
@@ -10,9 +10,14 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -33,9 +38,28 @@ public class FXMLDocumentController implements Initializable {
     private Button btnForsk; 
     
     @FXML
-    private void handleButtonAction(ActionEvent event) throws IOException {
-        System.out.println("You clicked me!");
-        label.setText("Hei");
+    private void forsikringKnapp(ActionEvent event) throws IOException {
+        Parent home_page_parent=FXMLLoader.load(getClass().getResource("/view/forsikringnivaa1.fxml"));
+        Scene home_page_scene=new Scene(home_page_parent);
+        Stage app_stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_page_scene);
+        app_stage.show();
+    }
+    @FXML
+    private void KundeKnapp(ActionEvent event) throws IOException {
+        Parent home_page_parent=FXMLLoader.load(getClass().getResource("/view/skjemaKunde.fxml"));
+        Scene home_page_scene=new Scene(home_page_parent);
+        Stage app_stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_page_scene);
+        app_stage.show();
+    }
+    @FXML
+    private void SkadeKnapp(ActionEvent event) throws IOException {
+        Parent home_page_parent=FXMLLoader.load(getClass().getResource("/view/skademeldingSkjema.fxml"));
+        Scene home_page_scene=new Scene(home_page_parent);
+        Stage app_stage= (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_page_scene);
+        app_stage.show();
     }
     
     @Override

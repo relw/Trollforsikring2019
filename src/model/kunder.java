ec86d130e8e50ccc4b01d4880f3cbeccf02936f4
aleksjoh <32634575+aleksjoh@users.noreply.github.com>
2019-04-10 15:42:58 +0200

    kunder

--- src/model/kunder.java
+++ src/model/kunder.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Aleksander
@@ -13,6 +15,36 @@ public class kunder {
 //• Skademeldinger
 //• Ubetalte erstatninger
 
-    //String 
+    private String datoforhold, navn, fakturaadresse; 
+    private int forsikringsnummer;
+    
+    private ArrayList<forsikringer> = new ArrayList<>(); 
+    private ArrayList<skademeldinger> = new ArrayList<>(); 
+    private ArrayList<ubetalteErstatninger> = new ArrayList<>();
+    
+    public kunder(String innNavn, String dato, String fakturaadresse, int forsknummer)
+    {
+        this.navn = innNavn; 
+        this.datoforhold = dato; 
+        this.fakturaadresse = fakturaadresse;
+        this.forsikringsnummer = forsknummer; 
+    }
+    
+    public String getNavn()
+    {
+        return navn; 
+    }
+    public String getDato()
+    {
+        return datoforhold; 
+    }
+    public String getFakturaAdresse()
+    {
+        return fakturaadresse; 
+    }
+    public int getForsikringsNummer()
+    {
+        return forsikringsnummer; 
+    }
     
 }

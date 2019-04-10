2d5e306f4ac513d3012426bf1a65f35929fd4365
aleksjoh <32634575+aleksjoh@users.noreply.github.com>
2019-04-10 15:21:17 +0200

    kunder klasse fil

---
+++ src/model/kunder.java
@@ -0,0 +1,18 @@
+package model;
+
+/**
+ *
+ * @author Aleksander
+ */
+public class kunder {
+//    Dato for opprettet kundeforhold
+//• Navn
+//• Fakturaadresse
+//• Forsikringsnummer
+//• Alle kundens forsikringer
+//• Skademeldinger
+//• Ubetalte erstatninger
+
+    //String 
+    
+}

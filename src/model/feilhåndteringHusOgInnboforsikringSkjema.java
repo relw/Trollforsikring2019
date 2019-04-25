a56382081d16cdad417bd03fa00daa9e0e7869dd
olsenr <olsenr@online.no>
2019-04-25 16:00:12 +0200

    endret tekst

--- src/model/feilhåndteringHusOgInnboforsikringSkjema.java
+++ src/model/feilhåndteringHusOgInnboforsikringSkjema.java
@@ -3,7 +3,7 @@ package model;
 public class feilhåndteringHusOgInnboforsikringSkjema {
    public static void sjekkForsikringspremie(String inntekst)throws feilTallInnput{
        if(inntekst.isEmpty()){
-           throw new feilTallInnput("Du må skrive inn et tall");
+           throw new feilTallInnput("Skriv inn tall");
        }
        try{
            int forsikringspremie=Integer.parseInt(inntekst);
@@ -17,95 +17,95 @@ public class feilhåndteringHusOgInnboforsikringSkjema {
     
                 }
                 else{
-                    throw new feilDatoInnput("Ikke korrekt skrevet inn dato");
+                    throw new feilDatoInnput("Ikke korrekt dato format");
                 }
         }
      public static void sjekkForsikringsBeløp(String inntekst)throws feilTallInnput{
        if(inntekst.isEmpty()){
-           throw new feilTallInnput("Du må skrive inn et tall");
+           throw new feilTallInnput("Skriv inn tall");
        }
        try{
            int forsikringspremie=Integer.parseInt(inntekst);
        }
        catch(Exception e){
-           throw new feilTallInnput("Du må skrive inn tall");
+           throw new feilTallInnput("Skriv inn tall");
        }
    }
    public static void sjekkAdresse(String inntekst) throws feilTekstInnput{
        if(inntekst.isEmpty()){
-           throw new feilTekstInnput("Du må skrive inn adresse");
+           throw new feilTekstInnput("Skriv inn adresse");
        }
    }
    public static void sjekkByggeår(String inntekst) throws feilTallInnput{
        if(inntekst.isEmpty()){
-           throw new feilTallInnput("Du må skrive inn byggeår");
+           throw new feilTallInnput("Skriv inn byggeår");
        }
        try{
            int byggeår=Integer.parseInt(inntekst);
        }
        catch(Exception e){
-           throw new feilDatoInnput("Du må skrive inn årstall");
+           throw new feilDatoInnput("Skriv inn årstall");
        }
        if(inntekst.length()!=4){
-            throw new feilDatoInnput("Du må skrive inn årstall");
+            throw new feilDatoInnput("Skriv inn årstall");
        } 
    }
    public static void sjekkBoligtype(String inntekst) throws feilTekstInnput{
         if(inntekst.isEmpty()){
-            throw new feilTekstInnput("Du må skrive inn boligtype");
+            throw new feilTekstInnput("Skriv inn boligtype");
         }
         if(inntekst.matches("[0-9]+")){
-            throw new feilTekstInnput("Det er ikke tall i boligtype");
+            throw new feilTekstInnput("Er ikke tall i boligtype");
         }
     }
   
    public static void sjekkByggemateriale(String inntekst) throws feilTekstInnput{
         if(inntekst.isEmpty()){
-            throw new feilTekstInnput("Du må skrive inn byggemateriale");
+            throw new feilTekstInnput("Skriv inn byggemateriale");
         }
         if(inntekst.matches("[0-9]+")){
-            throw new feilTekstInnput("Det er ikke tall i byggemateriale");
+            throw new feilTekstInnput("Er ikke tall i byggemateriale");
         }
     }
    public static void sjekkStandard(String inntekst) throws feilTekstInnput{
        if(inntekst.isEmpty()){
-           throw new feilTekstInnput("Du må skrive inn standard");
+           throw new feilTekstInnput("Skriv inn standard");
        }
        if(inntekst.matches("[0-9]+")){
-            throw new feilTekstInnput("Det er ikke tall i standard");
+            throw new feilTekstInnput("Er ikke tall i standard");
         }   
    }
   public static void sjekkKvm(String inntekst) throws feilTallInnput{
         if(inntekst.isEmpty()){
-            throw new feilTallInnput("Du må skrive inn antall kvm");
+            throw new feilTallInnput("Skriv antall kvm");
         }
         try{
             int kvm=Integer.parseInt(inntekst);
         }
         catch(Exception e){
-            throw new feilTallInnput("Du må skrive inn tall");
+            throw new feilTallInnput("Skriv inn tall");
         }
     }
   public static void sjekkForsikringsbeløpBygning(String inntekst) throws feilTallInnput{
       if(inntekst.isEmpty()){
-            throw new feilTallInnput("Du må skrive inn forsikrinsbeløp bygning");
+            throw new feilTallInnput("Skriv inn forsikrinsbeløp");
         }
         try{
             int kvm=Integer.parseInt(inntekst);
         }
         catch(Exception e){
-            throw new feilTallInnput("Du må skrive inn tall");
+            throw new feilTallInnput("Skriv inn tall");
         }
   }
    public static void sjekkForsikringsbeløpInnbo(String inntekst) throws feilTallInnput{
       if(inntekst.isEmpty()){
-            throw new feilTallInnput("Du må skrive inn forsikrinsbeløp bygning");
+            throw new feilTallInnput("Skriv inn forsikrinsbeløp");
         }
         try{
             int kvm=Integer.parseInt(inntekst);
         }
         catch(Exception e){
-            throw new feilTallInnput("Du må skrive inn tall");
+            throw new feilTallInnput("Skriv inn tall");
         }
   }
    

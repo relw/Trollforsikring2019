ab7dd2f4910f68b36059b8632ed5d730359dba15
olsenr <olsenr@online.no>
2019-04-25 16:03:04 +0200

    endret tekst

--- src/model/feilhåndteringReiseforsikringSkjema.java
+++ src/model/feilhåndteringReiseforsikringSkjema.java
@@ -2,13 +2,13 @@ package model;
 public class feilhåndteringReiseforsikringSkjema {
     public static void sjekkForsikringspremie(String inntekst) throws feilTallInnput{
         if(inntekst.isEmpty()){
-            throw new feilTallInnput("Du må skrive inn et tall");
+            throw new feilTallInnput("Skriv inn et tall");
         }
         try{
             int tall=Integer.parseInt(inntekst);
         }
         catch(Exception e){
-            throw new feilTallInnput("Du må skrive inn et tall");
+            throw new feilTallInnput("Skriv inn et tall");
         }
     }
      public static void sjekkInputDato(String inntekst) throws feilDatoInnput{
@@ -16,33 +16,33 @@ public class feilhåndteringReiseforsikringSkjema {
     
                 }
                 else{
-                    throw new feilDatoInnput("Ikke korrekt skrevet inn dato");
+                    throw new feilDatoInnput("Feil dato format");
                 }
         }
      public static void sjekkForsikringsbeløp(String inntekst)throws feilTallInnput{
         try{
             int tall=Integer.parseInt(inntekst);
         }catch(Exception e){
-                throw new feilTallInnput("Du må skrive inn tall");
+                throw new feilTallInnput("Skriv et tall");
                 }
         }
      
      public static void sjekkForsikringsOmråde(String inntekst) throws feilOmrådeInnput{
         if(inntekst.isEmpty()){
-            throw new feilOmrådeInnput("Du må skrive inn et navn");
+            throw new feilOmrådeInnput("Skriv inn navn");
         }
         if(inntekst.matches("[a-zA-Z]+")){
             
         }
         else{
-            throw new feilOmrådeInnput("Noe er feil med navnet ditt");
+            throw new feilOmrådeInnput("Feil med navnet");
         }
     }
      public static void sjekkForsikringsSum(String inntekst)throws feilTallInnput{
         try{
             int tall=Integer.parseInt(inntekst);
         }catch(Exception e){
-                throw new feilTallInnput("Du må skrive inn tall");
+                throw new feilTallInnput("Skriv inn tall");
                 }
         }
     public static class feilTallInnput extends Exception{

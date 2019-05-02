cb2b9f4fd23d78692e79623bf35ef62d952a10cf
aleksjoh <32634575+aleksjoh@users.noreply.github.com>
2019-05-02 15:56:53 +0200

    kl

--- src/controller/registerController.java
+++ src/controller/registerController.java
@@ -93,7 +93,7 @@ public class registerController implements Initializable {
                        obj2.pluss(k2);
                    }
                    skrivKundeFil.skrive(obj2);
-                   slettetInfo.setText("Kunden er slettet");
+                   slettetInfo.setText("Kunden er slettet!");
                 }
             }
            
